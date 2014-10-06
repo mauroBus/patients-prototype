4156544935bd9794999875bfb4a28522432eb9bf
Lisandro Luis Falconi <lisandrofalconi@gmail.com>
2014-10-06 10:11:35 -0300

    New tests

---
+++ test/protractor/pageObject/angularjsHomePage.js
@@ -0,0 +1,12 @@
+var AngularHomepage = function() {
+  this.nameInput = element(by.model('yourName'));
+  this.greeting = element(by.binding('yourName'));
+
+  this.get = function() {
+    browser.get('http://www.angularjs.org');
+  };
+
+  this.setName = function(name) {
+    this.nameInput.sendKeys(name);
+  };
+};
--- test/spec/node-spec.js
+++ test/spec/node-spec.js
@@ -1,13 +1,10 @@
 var request = require ('http');
 
 describe("Node server", function() {
-	
 	it("Server should respond to /", function() {
 		request.get('http://test_server',function (response) {
 			expect(response.statusCode).toBe(200);
 			done();
 		});
 	});
-
-
 });	
