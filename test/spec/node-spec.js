b078b70102c63c8ff58c86ad9ca8624ce6243a26
Lisandro Luis Falconi <lisandrofalconi@gmail.com>
2014-10-06 17:02:04 -0300

    New back-end tests
    Refactor, new tests, fixes. Use 'request' module instead of 'http'

--- test/spec/node-spec.js
+++ test/spec/node-spec.js
@@ -1,10 +1,61 @@
-var request = require ('http');
+var request = require("request");
+var mongoskin = require('mongoskin');
+
 
 describe("Node server", function() {
-	it("Server should respond to /", function() {
-		request.get('http://test_server',function (response) {
-			expect(response.statusCode).toBe(200);
+	var db = mongoskin.db('mongodb://@localhost:27017/mydb', {safe:true})
+
+	it("should respond cannot get /", function(done) {
+		request('http://localhost:8080',function (error, response, body) {
+			expect(response.statusCode).toEqual(404);
 			done();
 		});
 	});
+
+	beforeEach(function() {
+		db.collection('patients').remove({}, function (err,result) {
+			if (!err) console.log('Patients deleted!');
+		});
+
+	});
+
+	it("should respond empty body", function(done) {
+	
+		db.collection('patients').count(function (err,count) {
+			expect(count).toEqual(0);
+			done();
+		});	
+
+		request('http://localhost:8080/api/patients',function (error, response, body) {
+			expect(response.statusCode).toEqual(200);
+			expect(body).toEqual('[]');
+			done();
+		});
+	});
+
+	it('should insert a patient and respond success', function(done) {
+
+
+		var url = 'http://localhost:8080/api/patients';
+        var params = null;
+		params = {
+                url: url,
+    			json: true,
+    			body: {
+    				firstName: 'Pepe',
+					lastName: 'Argento',
+					dni:'1234567',
+					dob:'01-01-1967'
+    			}
+            };
+
+      	request.post(params,
+	    	function (error, response, body) {
+    		expect(response.statusCode).toEqual(200);
+	        expect(body.msg).toEqual('success');
+	        done();
+    	}
+	);	
+    });
+
 });	
