7d703d2fbaf1fe288574850a45c269e97b07abb9
Gabriel Lopez <glopez@devspark.com>
2014-10-07 11:18:15 -0300

    refactoring request handlers as plugable middleware

--- serverstatic/patients.js
+++ serverstatic/patients.js
@@ -3,9 +3,9 @@ var mongoskin = require('mongoskin');
 //mongodb connection
 var db = mongoskin.db('mongodb://@localhost:27017/mydb', {safe:true})
 
-module.exports = {
+module.exports = function(app){
 
-	listAllPatients: function(req, res){
+	app.get('/api/patients', function(req, res){
     	db.collection('patients').find({},{_id:0}).toArray(function (err, items) {
             if (err) {
                 console.log( err );
@@ -14,26 +14,34 @@ module.exports = {
             
             res.json(items);
     	});
-	},
+	});
 
-	addPatient:  function(req, res){
+    app.post('/api/patients', function(req, res, next){
+        //logging request
 		console.log('POST:');
 		console.log(req.body);
-
+        next();
+    }, function(req, res, next) {
+        //checking JSON sanity
         if (req.body && req.body.firstName && req.body.lastName && req.body.dni && req.body.dob) {
-            var dateParts = req.body.dob.split('-');
-            var date = new Date(dateParts[2], (dateParts[1] - 1), dateParts[0]);
-            if (date < new Date()){
-                db.collection('patients').insert(req.body, function(err, result){
-                    res.send( (err === null) ? { msg: 'success' } : { msg: 'db error!' });
-                });
-            } else {
-                res.send( { msg: 'error: future date of birth!' });
-            }
+            next();
         } else {
             res.send( { msg: 'error: missing fields' });
         }
-
-	}
-
+	}, function(req, res, next) {
+        //validating date of birth
+        var dateParts = req.body.dob.split('-');
+        var date = new Date(dateParts[2], (dateParts[1] - 1), dateParts[0]);
+        if (date < new Date()){
+            next();
+        } else {
+            res.send( { msg: 'error: future date of birth!' });
+        }
+    }, function(req, res,next){
+        //adding patient to collection
+        db.collection('patients').insert(req.body, function(err, result){
+                res.send( (err === null) ? { msg: 'success' } : { msg: 'db error!' });
+        });
+    });
+    
 }
