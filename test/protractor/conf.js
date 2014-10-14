934661ff6584312c20e80c9814dc263e694e6e33
Lisandro Luis Falconi <lisandrofalconi@gmail.com>
2014-10-14 17:39:29 -0300

    New protractor tests
    UI Angular tests with Protractor

--- test/protractor/conf.js
+++ test/protractor/conf.js
@@ -1,7 +1,7 @@
 // conf.js
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['spec.js'],
+  specs: ['patientsHomePageSpec.js'],
   capabilities: {
   	browserName: 'chrome'
   }
